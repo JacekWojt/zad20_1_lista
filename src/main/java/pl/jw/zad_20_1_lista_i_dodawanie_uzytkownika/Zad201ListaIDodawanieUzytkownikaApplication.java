2364ec2d222089dd63efeca6d9d8aa1ad3b7fa4f
Jacek Wojtuszek <jacek.wojtuszek@gmail.com>
2023-04-06 15:44:04 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/jw/zad_20_1_lista_i_dodawanie_uzytkownika/Zad201ListaIDodawanieUzytkownikaApplication.java
@@ -0,0 +1,13 @@
+package pl.jw.zad_20_1_lista_i_dodawanie_uzytkownika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad201ListaIDodawanieUzytkownikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zad201ListaIDodawanieUzytkownikaApplication.class, args);
+    }
+
+}
