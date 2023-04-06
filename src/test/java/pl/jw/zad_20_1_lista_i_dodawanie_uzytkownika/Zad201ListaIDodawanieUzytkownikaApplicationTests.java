427a8768d98345f47796dc6a358adf6fda40ef5f
Jacek Wojtuszek <jacek.wojtuszek@gmail.com>
2023-04-06 15:44:18 +0200

    Stworzenie projektu

---
+++ src/test/java/pl/jw/zad_20_1_lista_i_dodawanie_uzytkownika/Zad201ListaIDodawanieUzytkownikaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jw.zad_20_1_lista_i_dodawanie_uzytkownika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zad201ListaIDodawanieUzytkownikaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
